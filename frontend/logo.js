a68ad6f889e653e087966323b61243558c79afab
Harveer Dhillon <harveerdhillon91@gmail.com>
2025-07-05 21:25:33 -0400

    Changed the timing regarding the animation, to create a smoother animation and organized my html file for better readability

--- frontend/logo.js
+++ frontend/logo.js
@@ -6,7 +6,7 @@ function fade_logos(class_name){
         for (let logo of logos){
             logo.style.opacity = "0";
         }
-        setTimeout(fade_in, 12000);
+        setTimeout(fade_in, 16500);
     }
     function fade_in(){ 
         for (let logo of logos){
@@ -18,7 +18,7 @@ function fade_logos(class_name){
     fade_in();
 }
 fade_logos('team-logos1')
-setTimeout(() => { fade_logos('team-logos2'); }, 3000);
-setTimeout(() => { fade_logos('team-logos3'); }, 6000);
-setTimeout(() => { fade_logos('team-logos4'); }, 9000);
+setTimeout(() => { fade_logos('team-logos2'); }, 4500);
+setTimeout(() => { fade_logos('team-logos3'); }, 9000);
+setTimeout(() => { fade_logos('team-logos4'); }, 13500);
 
