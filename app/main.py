66bd64a879392a2656989f470653a35469ed52f5
Harveer Dhillon <harveerdhillon91@gmail.com>
2025-07-11 18:42:36 -0400

    fixing main

--- app/main.py
+++ app/main.py
@@ -26,32 +26,32 @@ def read_root():
 
 #Connects the api endpoints (Defines the pathways a user could potintially ask for and requests them from data_collector)
 #Data collector 
-@app.get("/team/{team_id}/roster")
+@app.get("/api/team/{team_id}/roster")
 def team_roster(team_id: int):
     return get_team_roster(team_id)
 
-@app.get("/team/{team_abbr}/stats")
+@app.get("/api/team/{team_abbr}/stats")
 def team_stats(team_abbr: str):
     return get_team_stats(team_abbr, "20242025")
 
-@app.get("/team/{team_abbr}/stats/now")
+@app.get("/api/team/{team_abbr}/stats/now")
 def team_skaters_now(team_abbr: str):
     return get_skater_stats_for_single_team(team_abbr)
 
-@app.get("/team/{team_abbr}/goalies")
+@app.get("/api/team/{team_abbr}/goalies")
 def team_goalies(team_abbr: str):
     return get_goalie_stats_one_team(team_abbr)
 
-@app.get("/player/{player_id}")
+@app.get("/api/player/{player_id}")
 def player_stats(player_id: int):
     return get_player_stats(player_id)
 
-@app.get("/stats-leaders")
+@app.get("/api/stats-leaders")
 def get_stat_leaders(category: str, limit: int = 25):
     result = stat_leaders(category, limit)  
     return result
 
-@app.get("/v1/draft/rankings/{season}/{prospect_category}")
+@app.get("/api/v1/draft/rankings/{season}/{prospect_category}")
 def rookie_rankings(season: int, prospect_category: int):
     response = requests.get(f"https://api-web.nhle.com/v1/draft/rankings/{season}/{prospect_category}")
     if response.status_code == 200:
